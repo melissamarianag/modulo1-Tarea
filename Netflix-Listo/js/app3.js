94c060553b0e8a8521b6fc80733a0e9cd6d9d7d3
Melissa G <melissawhitefox@gmail.com>
2022-03-09 18:17:03 -0600

    Tarea del modulo 1 Netflix y Portafolio

---
+++ Netflix-Listo/js/app3.js
@@ -0,0 +1,60 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista3'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores3',
+		arrows: {
+			prev: '.carousel__anterior3',
+			next: '.carousel__siguiente3'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 100,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},
+			{
+				// screens greater than >= 775px
+				breakpoint: 500,
+				settings: {
+				  // Set to `auto` and provide item width to adjust to viewport
+				  slidesToShow: 3,
+				  slidesToScroll: 3
+				}
+			  },
+
+			  {
+				// screens greater than >= 775px
+				breakpoint: 1000,
+				settings: {
+				  // Set to `auto` and provide item width to adjust to viewport
+				  slidesToShow: 4,
+				  slidesToScroll: 4
+				}
+			  },
+			  {
+				// screens greater than >= 775px
+				breakpoint: 1500,
+				settings: {
+				  // Set to `auto` and provide item width to adjust to viewport
+				  slidesToShow: 5,
+				  slidesToScroll: 5
+				}
+			  },
+			
+			
+			{
+			  // screens greater than >= 1024px
+			  breakpoint: 2000,
+			  settings: {
+				slidesToShow: 6,
+				slidesToScroll: 6
+			  }
+			}
+		]
+	});
+});
